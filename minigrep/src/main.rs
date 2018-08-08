af43d3e577de3c442c1c680bd55f417bca5ea724
Shreevari SP <shreevari.sp@gmail.com>
2018-08-08 14:10:42 +0530

    IOproject & Browser extensions

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,19 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+use std::process;
+
+extern crate minigrep;
+use minigrep::Config;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    println!("Searching for {}", query);
-    println!("In file : {}", filename);
-    let mut f = File::open(filename).expect("File not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-    	.expect("Something went wrong reading the file");
-    println!("With text : {}", contents);
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Problem parsing arguments : {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file : {}", config.filename);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error : {}", e);
+        process::exit(1);
+    }
 }
